d6e9a9922d11a0f152aeb9a4d3f9ea77f0f0fda5
Omar Fabian <sony.omar91@gmail.com>
2020-05-20 12:02:18 -0300

    herencia

--- electrostore/tiendaonline/views.py
+++ electrostore/tiendaonline/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'titulo': 'gracias por visitar Electro Store!'
+        #'producto': model.producto ej?
+    }
+    return render(request, 'index.html', context)
