677a5b2bcc63a57f6a0588eea98ec43160bfa9f5
hect20 <64923844+hect20@users.noreply.github.com>
2020-05-22 16:11:14 -0300

    creacion de base de datos sql3

--- electrostore/tiendaonline/models.py
+++ electrostore/tiendaonline/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre= models.CharField(max_length=30)
+
 class Usuario(models.Model):
     email= models.EmailField(max_length=40)
     password= models.CharField(max_length=30)
     nombre= models.CharField(max_length=30)
     apellido= models.CharField(max_length=20)
-    dni= models.PositiveIntegerField()
-    
+    dni= models.PositiveIntegerField()  
 
 class Producto(models.Model):
     titulo= models.CharField(max_length=60)
@@ -16,12 +18,13 @@ class Producto(models.Model):
     promocion= models.CharField(max_length=60)
     fecha_hora= models.DateField(auto_now=True)
     usuario= models.ManyToManyField(Usuario)
+    categoria= models.ForeignKey(Categoria, null=False, blank= False, on_delete= models.CASCADE)
 
-class Categoria(models.Model):
-    nombre= models.CharField(max_length=30)
+
 
 class Foto(models.Model):
     nombreArchivo= models.CharField(max_length=40)
+    producto= models.ForeignKey(Producto, null=False, blank= False, on_delete= models.CASCADE)
 
 class Compra(models.Model):
     fecha_hora= models.DateField(auto_now=True)
@@ -31,6 +34,8 @@ class Compra(models.Model):
 class Itemvendido(models.Model):
     fecha_hora= models.DateField(auto_now=True)
     precio= models.DecimalField(max_digits=20, decimal_places=2)
+    compra= models.ForeignKey(Compra,null=False, blank= False, on_delete= models.CASCADE)
+    producto= models.ForeignKey(Producto, null=False, blank= False, on_delete= models.CASCADE)
 
 class Administrador(models.Model):
     email= models.EmailField(max_length=40)
