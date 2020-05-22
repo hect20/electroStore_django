de8aa6da85907833d54a71298b578155f05a1242
hect20 <64923844+hect20@users.noreply.github.com>
2020-05-22 17:28:47 -0300

    creacion de fixture

---
+++ electrostore/fixture.py
@@ -0,0 +1,11 @@
+import json
+from random import randrange
+from electrostore.models import Usuario
+
+with open('USUARIOS.json', 'r') as fp:
+    usuarios = json.load(fp)
+
+for a in usuarios:
+    n = Usuario(email=a['email'], password=a['password'], nombre=a['nombre'], apellido=a['apellido'], dni=a['dni'])
+    n.save()
+    print('GUARDANDO USUARIO...')
