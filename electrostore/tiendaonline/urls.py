d3a7b6c4644d86348a304c0aa120596591dcbb2a
Omar Fabian <sony.omar91@gmail.com>
2020-05-22 15:30:12 -0300

    nada importante

--- electrostore/tiendaonline/urls.py
+++ electrostore/tiendaonline/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-
+from django.urls import path, re_path
 from . import views
+
+#app_name="tiendaonline_app"
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
