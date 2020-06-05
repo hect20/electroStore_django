1a204da99c2fc2f5c0b68aca8f89f85e3616c198
hect20 <64923844+hect20@users.noreply.github.com>
2020-06-05 15:03:41 -0300

    Update urls.py

--- electrostore/tiendaonline/urls.py
+++ electrostore/tiendaonline/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.index, name='home'),
     path('producto/' ,views.producto_view.as_view(), name='producto'),
     path('lista-producto/',views.listaProductos.as_view(), name='listaproducto'),
-    path('buscar/',views.buscarProducto, name='busquedaproducto'),
+    path('buscar/',views.buscarProducto, name='buscar'),
 
     ##
     path('materiales/',views.materiales, name='materiales'),
