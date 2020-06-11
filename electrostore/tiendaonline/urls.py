6e4f3758ae452dc3560d5b4127e8cf383dd3bb21
eugeluque06 <64925844+eugeluque06@users.noreply.github.com>
2020-06-10 21:28:29 -0300

    Add files via upload

--- electrostore/tiendaonline/urls.py
+++ electrostore/tiendaonline/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from tiendaonline import views
 
 urlpatterns = [
-    path('',views.index, name='home'),
+    path('',views.producto_promocion.as_view(), name='home'),
     path('carga_producto/' ,views.producto_view.as_view(), name='carga_producto'),
     path('lista_productos/',views.listaProductos.as_view(), name='listaproductos'),
     path('buscar/',views.buscarProducto, name='buscar'),
 
     ##
+    
     path('materiales/',views.materiales.as_view(), name='materiales'),
     path('herramientas/',views.herramientas.as_view(), name='herramientas'),
     path('componentes/',views.componentes.as_view(), name='componentes'),
