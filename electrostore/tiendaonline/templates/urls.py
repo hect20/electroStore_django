44fa05e3bbdaf4c4a55fd7628ffe96a38f1f6d95
eugeluque06 <64925844+eugeluque06@users.noreply.github.com>
2020-06-10 21:26:05 -0300

    Add files via upload

---
+++ electrostore/tiendaonline/templates/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from tiendaonline import views
+
+urlpatterns = [
+    path('',views.producto_promocion.as_view(), name='home'),
+    path('carga_producto/' ,views.producto_view.as_view(), name='carga_producto'),
+    path('lista_productos/',views.listaProductos.as_view(), name='listaproductos'),
+    path('buscar/',views.buscarProducto, name='buscar'),
+
+    ##
+    
+    path('materiales/',views.materiales.as_view(), name='materiales'),
+    path('herramientas/',views.herramientas.as_view(), name='herramientas'),
+    path('componentes/',views.componentes.as_view(), name='componentes'),
+    path('kits/',views.kits.as_view(), name='kits'),
+
+    #path('categorias',views.categorias.as_view(),name='categorias'),
+    ##
+    path('detalle-producto/<int:id>/',views.detalleProducto, name='detalleProducto'),
+
+
+    path('carro/',views.carrito, name='carro'),
+    
+    # omar - editar producto
+    path('lista_productos/eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('lista_productos/<int:id>/', views.editar_producto, name='editar_producto'),
+]
+
+
+
