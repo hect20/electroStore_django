ebdd899bec7e76098c8411d968db6453c3f5dc68
hect20 <64923844+hect20@users.noreply.github.com>
2020-06-24 01:00:37 -0300

    update

--- electrostore/cart/context_processors.py
+++ electrostore/cart/context_processors.py
@@ -2,10 +2,11 @@ from .cart import Cart
 
 def cart_total_amount(request):
 	if request.user.is_authenticated:
-		cart = Cart(request)
-		total_bill = 0.0
+		cart= Cart(request)
+		total_bill=0.0
 		for key,value in request.session['cart'].items():
-			total_bill = total_bill + (float(value['precio']) * value['quantity'])
-		return {'cart_total_amount' : total_bill} 
+			total_bill= total_bill + (float(value['precio']) * value['quantity'])
+		return {'cart_total_amount' : total_bill}
 	else:
-		return {'cart_total_amount' : 0} 
+		return {'cart_total_amount' : 0}
+
